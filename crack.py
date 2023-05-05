0d11f35723900d608b6cc9c0a45a79b0145d0648
TihamX <105205393+Tiham69@users.noreply.github.com>
2023-05-05 18:18:36 +0600

    Add files via upload

--- crack.py
+++ crack.py
@@ -1,7 +1,8 @@
 from string import digits, ascii_letters, punctuation, ascii_lowercase
 from itertools import product
 
-for passcode in product(digits, repeat=4): # can be used that was imported form string = ascii_lowercase, digits, punctuation, ascii_letters and many more
-        print(*passcode)
+for i in range(26):
+    for passcode in product(ascii_lowercase, repeat=i): # can be used that was imported form string = ascii_lowercase, digits, punctuation, ascii_letters and many more
+            print(*passcode)
 
 # This code can be useful to crack a phones password
